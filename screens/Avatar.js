8aebb9aab87ab67bcb7d05027b90627437bf675d
Andreac03 <andreamarcelac.s@gmail.com>
2024-05-23 20:50:40 -0500

    cambio

--- screens/Avatar.js
+++ screens/Avatar.js
@@ -34,7 +34,7 @@ function Avatar() {
                                 :
                                 <Text
                                     style={style.TextoAI}
-                                >AI
+                                >AI....
                                 </Text>
                             }
                         </View>
